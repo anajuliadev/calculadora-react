beea7ad8f0fa4a3a719a1a613cb9eaa0cf7bc0f2
Ana Júlia <anaju.silvaalves@gmail.com>
2025-09-09 21:26:15 -0300

    style: layout de calculadora

--- src/components/Button/style.js
+++ src/components/Button/style.js
@@ -1,1 +1,11 @@
 import styled from 'styled-components';
+
+export const ButtonContainer = styled.button`
+    padding: 20px;
+    border: 1px solid #ffffff;
+    background-color: #e2e2e2ff;
+    border: 1px solid #000000;
+    font-size: 24px;
+    font-weight: 700;
+    flex: 1;
+`
--- src/components/Input/style.js
+++ src/components/Input/style.js
@@ -5,21 +5,24 @@ export const InputContainer = styled.div`
     height: 75px;
     background-color: #202020ff;
 
-    displey: flex;
+    display: flex;
     align-items: center;
     justify-content: flex-end;
 
     font-size: 24px;
     font-family: 'Roboto';
-
+    
+    
     input {
         width: 100%;
-        height: 75px;
+        height: 100%;
         background-color: #bebebeff;
         border: 0;
         display: flex;
         flex-direction: column;
         align-items: flex-end;
+        padding: 0 10px;
+        box-sizing: border-box;
         
         font-size: 24px;
         font-family: 'Roboto';
