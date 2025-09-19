e335fbc1c7e0b54d00d0cc6d8f3ae89a5e21f4c4
Ana Júlia <anaju.silvaalves@gmail.com>
2025-09-18 21:17:05 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,48 +2,66 @@ import Input from './components/Input';
 import Button from './components/Button';
 
 import { Container, Content, Row } from './styles';
-import { use, useState } from 'react';
+import { useState } from 'react';
 
 
 const App = () => {
-  const [currentNumber, setCurrentNumber] = useState("0");
+  const [currentNumber, setCurrentNumber] = useState('0');
+  const [firstNumber, setFirstNumber] = useState('0');
 
+  //limpando os numeros - voltando para o estado que foi definido, 0
   const handleAddNumber = (num) => {
-    setCurrentNumber(prev => `${num}${prev}`);
-    console.log('number');
+    setCurrentNumber(prev => `${prev === '0' ? '' : prev}${num}`);
   };
 
-return (
-  <Container>
-    <Content>
-      <Input value={currentNumber} />
-      <Row>
-        <Button label="x" onClick={() => handleAddNumber = ('x')} />
-        <Button label="/" />
-        <Button label="C" />
-        <Button label="X" />
-      </Row>
-      <Row>
-        <Button label="7" onClick={() => handleAddNumber = ('7')} />
-        <Button label="8" onClick={() => handleAddNumber = ('8')} />
-        <Button label="9" onClick={() => handleAddNumber = ('9')} />
-        <Button label="-" />
-      </Row>
-      <Row>
-        <Button label="4" onClick={() => handleAddNumber = ('4')} />
-        <Button label="5" onClick={() => handleAddNumber = ('5')} />
-        <Button label="6" onClick={() => handleAddNumber = ('6')} />
-        <Button label="+" />
-      </Row>
-      <Row>
-        <Button label="1" onClick={() => handleAddNumber = ('1')} />
-        <Button label="2" onClick={() => handleAddNumber = ('2')} />
-        <Button label="3" onClick={() => handleAddNumber = ('3')} />
-        <Button label="=" />
-      </Row>
-    </Content>
-  </Container>
-);
+  //limpando os numeros - voltando para o estado que foi definido, 0
+  const handleOnClear = () => {
+    setCurrentNumber('0')
+    setFirstNumber('0')
+  }
+
+  //somando os numeros
+  const handleSumNumbers = () => {
+    if (firstNumber === '0') {
+      setFirstNumber(String(currentNumber));
+      setCurrentNumber('0');    
+    } else {
+      const sum = Number(firstNumber) + Number(currentNumber);
+      setCurrentNumber(String(sum));
+    }
+  }
+
+  return (
+    <Container>
+      <Content>
+        <Input value={currentNumber} />
+        <Row>
+          <Button label="x" onClick={() => handleAddNumber('x')} />
+          <Button label="/" />
+          <Button label="C" onClick={handleOnClear} />
+          <Button label="X" />
+        </Row>
+        <Row>
+          <Button label="7" onClick={() => handleAddNumber('7')} />
+          <Button label="8" onClick={() => handleAddNumber('8')} />
+          <Button label="9" onClick={() => handleAddNumber('9')} />
+          <Button label="-" />
+        </Row>
+        <Row>
+          <Button label="4" onClick={() => handleAddNumber('4')} />
+          <Button label="5" onClick={() => handleAddNumber('5')} />
+          <Button label="6" onClick={() => handleAddNumber('6')} />
+          <Button label="+" onClick={handleSumNumbers} />
+        </Row>
+        <Row>
+          <Button label="1" onClick={() => handleAddNumber('1')} />
+          <Button label="2" onClick={() => handleAddNumber('2')} />
+          <Button label="3" onClick={() => handleAddNumber('3')} />
+          <Button label="=" onClick={() => handleAddNumber('')} />
+        </Row>
+      </Content>
+    </Container>
+  );
 }
 
 export default App;
